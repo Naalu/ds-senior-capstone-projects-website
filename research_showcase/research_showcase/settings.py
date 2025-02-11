10a9a196a33976d8047536c4e5ae1d9f854d9531
Chris Reger <59154050+Naalu@users.noreply.github.com>
2025-02-11 16:58:27 -0700

    Implemented and tested basic user authentication

--- research_showcase/research_showcase/settings.py
+++ research_showcase/research_showcase/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = "research_showcase.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -80,6 +81,8 @@ DATABASES = {
     }
 }
 
+# Use our custom user model
+AUTH_USER_MODEL = "users.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
