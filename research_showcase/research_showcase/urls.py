2945a0e35e9daed046af066af0480fb4fe375585
ewf25 <ewf25@nau.edu>
2025-03-26 21:08:29 -0700

    Revert "Update urls.py"
    This reverts commit 567aa3e6d508e10e3c183b0de8a48f197c390b49.

--- research_showcase/research_showcase/urls.py
+++ research_showcase/research_showcase/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
     path("research/", include("research.urls")),
-    path("search/", include("search.urls"), name="search"),
+    path("search/", views.search_page, name="search"),
 ]
